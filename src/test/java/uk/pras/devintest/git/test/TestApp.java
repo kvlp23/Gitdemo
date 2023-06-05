518b6d0392b2f32bc95221e00066cab53e33879a
Pras <prasnuka@gmail.com>
2023-06-04 23:06:23 -0700

    Jira 1234-Adding project to SCM the first time

---
+++ src/test/java/uk/pras/devintest/git/test/TestApp.java
@@ -0,0 +1,10 @@
+package uk.pras.devintest.git.test;
+
+import org.testng.annotations.Test;
+
+public class TestApp {
+    @Test
+    public void test1(){
+        System.out.println("Running Test1");
+    }
+}
