fda78baa2011d98d8ce0a02e21d12548154b6bb9
Pras <prasnuka@gmail.com>
2023-06-07 06:04:33 -0700

    Jira 5678-Added private methods to both src files

--- src/main/java/uk/pras/devintest/git/Application.java
+++ src/main/java/uk/pras/devintest/git/Application.java
@@ -4,4 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Git is cool");
     }
+    private void doSomething(){
+        System.out.println("doing something");
+    }
 }
--- src/test/java/uk/pras/devintest/git/test/TestApp.java
+++ src/test/java/uk/pras/devintest/git/test/TestApp.java
@@ -6,5 +6,11 @@ public class TestApp {
     @Test
     public void test1(){
         System.out.println("Running Test1");
+
+    }
+
+    private void doSomething(){
+        System.out.println("doing something");
     }
 }
+
